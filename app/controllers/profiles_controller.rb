a2a933cc157332bc5e427fbf4a1eb49cd064178b
Chase Covington <mchasecov@gmail.com>
2021-11-28 15:29:54 -0500

    Add: profiles controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,11 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
