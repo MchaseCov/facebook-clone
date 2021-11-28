ba71d1b1cef8ac8a5b749fbed426c8a8ddd6d7ca
Chase Covington <mchasecov@gmail.com>
2021-11-28 15:37:32 -0500

    add: full name and nickname forms to devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :permitted_params, if: :devise_controller?
+
+  protected
+
+  def permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name nick_name])
+  end
 end
