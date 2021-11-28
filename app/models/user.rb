4e69d8744e77fecabe65ca290405846add7000e5
Chase Covington <mchasecov@gmail.com>
2021-11-28 17:49:52 -0500

    Add: basic associations between users and friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # Database Schema:
+  # User Schema:
   # id:                 integer
   # email:              string
   # full_name:          string
@@ -15,4 +15,15 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :full_name, presence: true
   validates :nick_name, presence: true, length: { maximum: 50 }
+
+  # Associations
+
+  # Friend Requests
+  has_many :friend_requests,
+  foreign_key: :requesting_user_id,
+  dependent: :destroy
+
+  has_many :requested_friends,
+  through: :friend_requests,
+  source: :recieving_user
 end
