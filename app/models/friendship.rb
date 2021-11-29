e44d1667880a090036fa5e5682fcfeeb4b2c6132
Chase Covington <mchasecov@gmail.com>
2021-11-29 13:59:44 -0500

    Add: friendship validation for uniquness

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -9,8 +9,22 @@ class Friendship < ApplicationRecord
   # Scopes
   scope :friends, -> { where('status =?', true) }
   scope :not_friends, -> { where('status =?', false) }
-  # Associations
 
+  # Validations
+  validates :sent_to_id, uniqueness: { scope: :sent_by_id }
+  validate :cannot_friend_self
+
+  # Associations
   belongs_to :sent_to, class_name: 'User', foreign_key: 'sent_to_id'
   belongs_to :sent_by, class_name: 'User', foreign_key: 'sent_by_id'
+
+  # Methods
+
+  private
+
+  def cannot_friend_self
+    return unless sent_to_id == sent_by_id
+
+    errors.add(:base, 'You cannot add yourself as a friend!')
+  end
 end
