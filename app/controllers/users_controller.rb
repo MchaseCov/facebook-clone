46ce3b6f3d5125c51feb0f2e93ec068081135a5f
Chase Covington <mchasecov@gmail.com>
2021-11-29 15:40:42 -0500

    refactor: Change Profiles to Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class ProfilesController < ApplicationController
+class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
-  resources :posts, :profiles, :friend_requests
+  resources :users, only: %i[index show] do
+    resources :friendships, only: %i[create]
+  end
+  resources :posts
   post 'friend_requests/accept/:id', to: 'friend_requests#accept_request', as: 'accept_friend_request'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
