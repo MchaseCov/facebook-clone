809512e9013e89a42014d3442f542b3e9f29f565
Chase Covington <mchasecov@gmail.com>
2021-11-29 16:02:26 -0500

    add: friend request logic

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,52 @@
 class FriendshipsController < ApplicationController
+  before_action :fetch_recieving_user, only: %i[create]
+  before_action :fetch_existing_request, only: %i[accept_friend_request decline_friend_request]
+
+  # Creates a new friendship entry between the two users
   def create
+    return if friend_request_sent?(@recieving_user)
+    return if friend_request_recieved?(@recieving_user)
+
+    @new_friendship = current_user.sent_friend_request.build(sent_to_id: @recieving_user.id)
+    if @new_friendship.save
+      flash[:notice] = 'Friend Request Sent!'
+    else
+      flash[:error] = 'Friend Request Failed!'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  # Sets friendship status to true & creates an inverse friendship relation
+  def accept_friend_request
+    return unless @friendship
+
+    @friendship.status = true
+    if @friendship.save
+      flash[:notice] = 'Friend Request Accepted!'
+      @friendship_inverse = current_user.friend_request_sent.build(sent_to_id: params[:user_id], status: true)
+      @friendship_inverse.save
+    else
+      flash[:error] = 'Could not accept request!'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  # Declines friend request, deleting friendship entry
+  def decline_friend_request
+    return unless @friendship
+
+    @friendship.destroy
+    flash[:notice] = 'Friend Request Declined!'
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def fetch_recieving_user
+    @recieving_user = User.find(params[:user_id])
+  end
+
+  def fetch_existing_request
+    @friendship = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id, status: false)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
-  resources :users, only: %i[index show] do
-    resources :friendships, only: %i[create]
-  end
   resources :posts
-  post 'friend_requests/accept/:id', to: 'friend_requests#accept_request', as: 'accept_friend_request'
+
+  resources :users, only: %i[index show] do
+    resources :friendships, only: %i[create] do
+      collection do
+        get 'accept_friend_request'
+        get 'decline_friend_request'
+      end
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
