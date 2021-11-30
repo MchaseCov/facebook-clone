71c6baec4de4530161a769d0558d2c2f2884c5fe
Chase Covington <mchasecov@gmail.com>
2021-11-30 00:35:56 -0500

    clean: add comment to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,15 @@ class User < ApplicationRecord
   # Validations
   validates :email, presence: true
   validates :full_name, presence: true
-  validates :nick_name, presence: true, length: { maximum: 50 }
+  validates :nick_name, presence: true,
+                        length: { maximum: 50 }
   validates :avatar, content_type: %i[png jpg jpeg],
                      size: { less_than: 2.megabytes, message: 'must be less than 2MB in size' }
   validates :banner, content_type: %i[png jpg jpeg],
                      size: { less_than: 3.megabytes, message: 'must be less than 3MB in size' },
                      aspect_ratio: :is_16_9
   # Associations
+  #   Friendship
   has_many :friend_sent, class_name: 'Friendship',
                          foreign_key: 'sent_by_id',
                          inverse_of: 'sent_by',
@@ -43,6 +45,7 @@ class User < ApplicationRecord
   has_many :received_requests, -> { merge(Friendship.not_friends) },
            through: :friend_recieved,
            source: :sent_by
+  #   Active Storage
   has_one_attached :avatar
   has_one_attached :banner
 
