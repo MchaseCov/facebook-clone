39a6dfde3570bedd938f2ec1ea73292f2c3e65df
Chase Covington <mchasecov@gmail.com>
2021-12-02 20:41:22 -0500

    Refactor: Hotwire usage on User profile
    still needs testing

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module OdinFacebook
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_view.form_with_generates_remote_forms = false
   end
 end
