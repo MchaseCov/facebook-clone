108d1424df23d4f50ab9ac29a96d2631f92390e4
Chase Covington <mchasecov@gmail.com>
2021-12-10 18:34:06 -0500

    Refactor: improved route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,11 @@ Rails.application.routes.draw do
 
   # GROUPS [Journals, Membership]
   resources :groups do
-    resources :journals, only: %i[index new create], module: :groups
     member do
       get 'members' # 'members' here refers to a URL & respective controller action, not the route method member.
     end
     match 'users/:id', to: 'groups#toggle_membership', via: %i[put delete], as: 'toggle_membership'
+    resources :journals, only: %i[index new create], module: :groups
   end
 
   # JOURNALS [journal likes, comments]
@@ -31,10 +31,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: %i[index show] do
-    resources :journals, only: %i[index new create], module: :users
     member do # NOTE: may be worth refactoring these back into their respective controller with turbo tag partials
       get 'groups'
       get 'friendships'
+      get 'images'
     end
     resources :friendships, only: %i[create destroy] do
       collection do
@@ -42,13 +42,17 @@ Rails.application.routes.draw do
         get 'decline_friend_request'
       end
     end
+    resources :journals, only: %i[index new create], module: :users
   end
 
   # FRIEND REQUEST LIST
   resources :friendships, only: [:index]
 
   # NOTIFICATIONS LIST
-  resources :notifications, only: %i[index destroy]
+  resources :notifications, only: %i[index destroy] do
+    member do
+      post 'read_all'
+    end
+  end
   match 'notifications', to: 'notifications#read', via: [:post]
-  match 'notifications/read_all', to: 'notifications#read_all', via: [:post], as: 'notifications_read_all'
 end
